db1e94b41c022adac2899fbbfd6fe905eb70d0a0
majieat126 <majieat126@163.com>
2017-03-06 14:00:36 +0800

    测编码问题

--- src/com/git/demo/GitDemo.java
+++ src/com/git/demo/GitDemo.java
@@ -5,6 +5,8 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("我进入git世界！！！");
+		
+		System.out.println("编码问题gbk");
 	}
 
 }
