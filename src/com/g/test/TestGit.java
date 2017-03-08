5d8d6902c9ae90dc5e8b05c5b2b2474d62f1b13a
majie <majieat126@163.com>
2017-03-08 23:45:11 +0800

    rr

--- src/com/g/test/TestGit.java
+++ src/com/g/test/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("nnnnnntttqqqn");
-		System.out.println("tttttttt");
+		System.out.println("aatttttttt");
 	}
 
 }
